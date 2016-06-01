18fa96520994fbac6e8813f7d2ca3e36415b29ea
Robert Van Voorhees <rcvanvo@gmail.com>
2016-06-01 12:09:46 -0400

    Pulled entry out into its own class for easier manipulating output format.

--- src/main/java/com/planetvoor/DataTransformService.java
+++ src/main/java/com/planetvoor/DataTransformService.java
@@ -1,6 +1,7 @@
 package com.planetvoor;
 
 import com.opencsv.CSVWriter;
+import com.planetvoor.domain.Entry;
 import com.planetvoor.domain.MovieEntity;
 import com.planetvoor.domain.RatingEntity;
 import com.planetvoor.domain.UserEntity;
@@ -22,9 +23,6 @@ import java.net.URI;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author voor
@@ -132,35 +130,7 @@ public class DataTransformService {
         }
         try (CSVWriter writer = new CSVWriter(new FileWriter(output.getFile()), ',')) {
 
-            List<String> headers = new LinkedList<>();
-            headers.add("RECOMMENDED");
-            headers.add("RATING");
-            headers.add("USER_AGE");
-            headers.add("USER_MALE");
-            headers.add("USER_JOB");
-            headers.add("USER_ZIP");
-            headers.add("MOVIE_NAME");
-            headers.add("unknown");
-            headers.add("action");
-            headers.add("adventure");
-            headers.add("animation");
-            headers.add("children");
-            headers.add("comedy");
-            headers.add("crime");
-            headers.add("documentary");
-            headers.add("drama");
-            headers.add("fantasy");
-            headers.add("filmnoir");
-            headers.add("horror");
-            headers.add("musical");
-            headers.add("mystery");
-            headers.add("romance");
-            headers.add("scifi");
-            headers.add("thriller");
-            headers.add("war");
-            headers.add("western");
-
-            writer.writeNext(StringUtils.toStringArray(headers));
+            writer.writeNext(StringUtils.toStringArray(Entry.headers()));
 
             for (RatingEntity rating : ratingRepository.findAll()) {
                 UserEntity user = userRepository.findOne(rating.getUserId());
@@ -190,42 +160,7 @@ public class DataTransformService {
     }
 
     protected void writeEntry(CSVWriter writer, RatingEntity rating, UserEntity user, MovieEntity movie) {
-        List<Object> entry = new LinkedList<>();
 
-        Long rate = rating.getRating();
-        if (rate > 3) {
-            entry.add("1");
-        }
-        else {
-            entry.add("0");
-        }
-        entry.add(rating.getRating());
-
-        entry.add(user.getAge());
-        entry.add(UserEntity.Gender.MALE.equals(user.getGender()) ? "1" : "0");
-        entry.add(user.getJob());
-        entry.add(user.getZip());
-        entry.add(movie.getTitle());
-        entry.add(movie.getUnknown() ? "1" : "0");
-        entry.add(movie.getAction() ? "1" : "0");
-        entry.add(movie.getAdventure() ? "1" : "0");
-        entry.add(movie.getAnimation() ? "1" : "0");
-        entry.add(movie.getChildren() ? "1" : "0");
-        entry.add(movie.getComedy() ? "1" : "0");
-        entry.add(movie.getCrime() ? "1" : "0");
-        entry.add(movie.getDocumentary() ? "1" : "0");
-        entry.add(movie.getDrama() ? "1" : "0");
-        entry.add(movie.getFantasy() ? "1" : "0");
-        entry.add(movie.getFilmnoir() ? "1" : "0");
-        entry.add(movie.getHorror() ? "1" : "0");
-        entry.add(movie.getMusical() ? "1" : "0");
-        entry.add(movie.getMystery() ? "1" : "0");
-        entry.add(movie.getRomance() ? "1" : "0");
-        entry.add(movie.getScifi() ? "1" : "0");
-        entry.add(movie.getThriller() ? "1" : "0");
-        entry.add(movie.getWar() ? "1" : "0");
-        entry.add(movie.getWestern() ? "1" : "0");
-
-        writer.writeNext(StringUtils.toStringArray(entry.stream().map(x -> String.valueOf(x)).collect(Collectors.toList())));
+        writer.writeNext(StringUtils.toStringArray(Entry.builder().rating(rating).user(user).movie(movie).build().fields()));
     }
 }
---
+++ src/main/java/com/planetvoor/domain/Entry.java
@@ -0,0 +1,93 @@
+package com.planetvoor.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author voor
+ */
+@Data
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Entry {
+
+    private final RatingEntity rating;
+    private final UserEntity user;
+    private final MovieEntity movie;
+
+    public List<String> fields() {
+        List<Object> entry = new LinkedList<>();
+
+        if (rating.getRating() > 3) {
+            entry.add("1");
+        }
+        else {
+            entry.add("0");
+        }
+        entry.add(rating.getRating());
+
+        entry.add(user.getAge());
+        entry.add(UserEntity.Gender.MALE.equals(user.getGender()) ? "1" : "0");
+        entry.add(user.getJob());
+        entry.add(user.getZip());
+        entry.add(movie.getTitle());
+        entry.add(movie.getUnknown() ? "1" : "0");
+        entry.add(movie.getAction() ? "1" : "0");
+        entry.add(movie.getAdventure() ? "1" : "0");
+        entry.add(movie.getAnimation() ? "1" : "0");
+        entry.add(movie.getChildren() ? "1" : "0");
+        entry.add(movie.getComedy() ? "1" : "0");
+        entry.add(movie.getCrime() ? "1" : "0");
+        entry.add(movie.getDocumentary() ? "1" : "0");
+        entry.add(movie.getDrama() ? "1" : "0");
+        entry.add(movie.getFantasy() ? "1" : "0");
+        entry.add(movie.getFilmnoir() ? "1" : "0");
+        entry.add(movie.getHorror() ? "1" : "0");
+        entry.add(movie.getMusical() ? "1" : "0");
+        entry.add(movie.getMystery() ? "1" : "0");
+        entry.add(movie.getRomance() ? "1" : "0");
+        entry.add(movie.getScifi() ? "1" : "0");
+        entry.add(movie.getThriller() ? "1" : "0");
+        entry.add(movie.getWar() ? "1" : "0");
+        entry.add(movie.getWestern() ? "1" : "0");
+
+        return entry.stream().map(x -> String.valueOf(x)).collect(Collectors.toList());
+    }
+
+    public static List<String> headers() {
+        List<String> headers = new LinkedList<>();
+        headers.add("RECOMMENDED");
+        headers.add("RATING");
+        headers.add("USER_AGE");
+        headers.add("USER_MALE");
+        headers.add("USER_JOB");
+        headers.add("USER_ZIP");
+        headers.add("MOVIE_NAME");
+        headers.add("unknown");
+        headers.add("action");
+        headers.add("adventure");
+        headers.add("animation");
+        headers.add("children");
+        headers.add("comedy");
+        headers.add("crime");
+        headers.add("documentary");
+        headers.add("drama");
+        headers.add("fantasy");
+        headers.add("filmnoir");
+        headers.add("horror");
+        headers.add("musical");
+        headers.add("mystery");
+        headers.add("romance");
+        headers.add("scifi");
+        headers.add("thriller");
+        headers.add("war");
+        headers.add("western");
+        return headers;
+    }
+}
